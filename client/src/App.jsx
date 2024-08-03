25c2bac303d97f282415fb8513b9e71be5cdb265
jvopereira <jvopereira2004@gmail.com>
2024-08-03 02:05:52 -0300

    display initial route reponse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:3000/movies")
+      .then(response => setMovies(response.data))
+      .catch(error => console.error("Error fetching movies:", error));
+  }, []);
+
   return (
-      <h1>LetterLens</h1>
-  )
+    <div>
+      <h1>Random Movies</h1>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>{movie.title} ({movie.year})</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
