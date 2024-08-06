002d248b5ccc4be42ae7180de6128683f93b1029
jvopereira <jvopereira2004@gmail.com>
2024-08-05 22:02:00 -0300

    initial front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './index.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:3000/movies")
-      .then(response => setMovies(response.data))
+    axios.get("http://127.0.0.1:3000/server/movies/popular")
+      .then(response => setMovies(response.data.results))
       .catch(error => console.error("Error fetching movies:", error));
   }, []);
 
   return (
-    <div>
-      <h1>Random Movies</h1>
-      <ul>
-        {movies.map((movie, index) => (
-          <li key={index}>{movie.title} ({movie.year})</li>
-        ))}
-      </ul>
+    <div className='m-2 inline-block gap-3'>
+      <h1 className='text-3xl'>Popular movies</h1>
+      <div className=''>
+        <ul>
+          {movies.map((movie, index) => (
+            <>
+              <li key={index} className='float-right gap-6'>{movie.title} ({movie.release_date})</li>
+              <img src={"https://image.tmdb.org/t/p/original" + movie.poster_path} alt={movie.title + "Poster"} className='w-32 m-3'/>
+            </>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
